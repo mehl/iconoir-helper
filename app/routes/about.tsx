9cc3861b9c79d0919434ce062aa7d52b4e9121d6
Bastian Frank <git@bastian-frank.de>
2025-11-20 22:22:34 +0100

    text changes

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -10,17 +10,21 @@ export default function AboutRoute() {
         About ICONOIR Helper{" "}
       </h1>
       <p>ICONOIR Helper is a tool designed to help you browse, select, and generate CSS for ICONOIR icons easily.</p>
-      <p>Created by <a href="https://bastian-frank.de/">Bastian Frank</a> (<a href="https://github.com/mehl" target="_blank" rel="noopener noreferrer">@mehl</a>) for his own use.</p>
+      <p>Created by <a href="https://bastian-frank.de/">Bastian Frank</a> (<a href="https://github.com/mehl" target="_blank" rel="noopener noreferrer">@mehl</a>) for own use.</p>
       <p>May it be useful for you as well!</p>
       <p>For more information, visit the <a href="https://iconoir.com" target="_blank" rel="noopener noreferrer">ICONOIR website</a>.</p>
       <br />
       <h2>Contact Information</h2>
       <p>
         Bastian Frank<br />
-        Fregestraße 73<br />
-        12159 Berlin<br />
-        Germany<br />
+        <a href="https://bastian-frank.de" target="_blank" rel="noopener noreferrer">https://bastian-frank.de</a><br />
         Email: <a href="mailto:iconoir-helper@bastian-frank.de">iconoir-helper@bastian-frank.de</a>
+      </p>
+      <p>
+        <a href="https://github.com/mehl/iconoir-helper" target="_blank" rel="noopener noreferrer" className="d-inline-flex align-items-center gap-1">
+          <i className="iconoir-github" style={{ fontSize: "1.5rem" }}></i>
+          https://github.com/mehl/iconoir-helper
+        </a>
       </p>
     </div>
   </PageLayout>;
